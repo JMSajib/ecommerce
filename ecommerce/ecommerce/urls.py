2489ea0d20ba8585e403d1d034277d2f8a0ab08e
JMSajib <jm.sajib012@gmail.com>
2018-03-10 02:29:36 +0600

    Guest Login added

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -4,13 +4,14 @@ from django.conf.urls.static import static
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
-from accounts.views import login_page,register_page
+from accounts.views import login_page,register_page,guest_register_view
 from .views import home_page
 
 urlpatterns = [
     url(r'^$',home_page,name="home"),
     url(r'^login/',login_page,name="login"),
     url(r'^logout/',LogoutView.as_view(),name="logout"),
+    url(r'^register/guest/',guest_register_view,name="guest_register"),
     url(r'^register/',register_page,name="register"),
     url(r'^products/',include("products.urls",namespace="products")),
     url(r'^search/',include("search.urls",namespace="search")),
