510a26747e128ed554e3174b05f5c480a5b284f6
JMSajib <jm.sajib012@gmail.com>
2018-03-11 01:38:15 +0600

    Created addresses app

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
+from addresses.views import checkout_address_create_view
 from accounts.views import login_page,register_page,guest_register_view
 from .views import home_page
 
@@ -12,6 +13,7 @@ urlpatterns = [
     url(r'^login/',login_page,name="login"),
     url(r'^logout/',LogoutView.as_view(),name="logout"),
     url(r'^register/guest/',guest_register_view,name="guest_register"),
+    url(r'^checkout/address/create/$',checkout_address_create_view,name="checkout_address_create"),
     url(r'^register/',register_page,name="register"),
     url(r'^products/',include("products.urls",namespace="products")),
     url(r'^search/',include("search.urls",namespace="search")),
