6d53ea40c7d7467c78eef4800a00cf802b95efb4
JMSajib <jm.sajib012@gmail.com>
2018-03-15 00:29:12 +0600

    Finalize checkout button is done

--- ecommerce/orders/models.py
+++ ecommerce/orders/models.py
@@ -22,7 +22,9 @@ class OrderManager(models.Manager):
         else:
             obj = self.model.objects.create(
                 billing_profile=billing_profile,
-                cart=cart_obj
+                cart=cart_obj,
+                active=True,
+                status = "created"
             )
             created=True
         return obj, created
@@ -53,6 +55,24 @@ class Order(models.Model):
         self.total = formatted_total
         self.save()
         return new_total
+
+    def check_done(self):
+        billing_profile = self.billing_profile
+        shipping_address = self.shipping_address
+        billing_address = self.billing_address
+        total = self.total
+        if(billing_profile and shipping_address and billing_address and total > 0):
+            return True
+        return False
+
+    def mark_paid(self):
+        if(self.check_done()):
+            self.status = "paid"
+            self.save()
+        return self.status
+
+
+
 
 def pre_save_create_order_id(sender,instance,*args,**kwargs):
     if(not instance.order_id):
