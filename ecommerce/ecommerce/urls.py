8b6aad0b4126a6a05ad8c665dd69822b19d9edfe
JMSajib <jm.sajib012@gmail.com>
2018-03-19 01:23:23 +0600

    Finalize checkout done

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
-from addresses.views import checkout_address_create_view
+from addresses.views import checkout_address_create_view,checkout_address_reuse_view
 from accounts.views import login_page,register_page,guest_register_view
 from .views import home_page
 
@@ -14,6 +14,7 @@ urlpatterns = [
     url(r'^logout/',LogoutView.as_view(),name="logout"),
     url(r'^register/guest/',guest_register_view,name="guest_register"),
     url(r'^checkout/address/create/$',checkout_address_create_view,name="checkout_address_create"),
+    url(r'^checkout/address/reuse/$',checkout_address_reuse_view,name="checkout_address_reuse"),
     url(r'^register/',register_page,name="register"),
     url(r'^products/',include("products.urls",namespace="products")),
     url(r'^search/',include("search.urls",namespace="search")),
