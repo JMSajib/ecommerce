7dc4e52632c9fb98f78dadae336493563c3cd94b
JMSajib <jm.sajib012@gmail.com>
2018-03-28 22:27:48 +0600

    Customize User model

--- ecommerce/accounts/models.py
+++ ecommerce/accounts/models.py
@@ -5,13 +5,14 @@ from django.contrib.auth.models import (
 )
 
 class UserManager(BaseUserManager):
-    def create_user(self,email,password=None,is_active=True,is_staff=False,is_admin=False):
+    def create_user(self,email,full_name=None,password=None,is_active=True,is_staff=False,is_admin=False):
         if(not email):
             raise ValueError("User must have an email Address")
         if(not password):
             raise ValueError("User must have a password")
         user_obj = self.model(
-            email = self.normalize_email(email)
+            email = self.normalize_email(email),
+            full_name = full_name
         )
         user_obj.set_password(password)
         user_obj.staff  = is_staff
@@ -20,17 +21,19 @@ class UserManager(BaseUserManager):
         user_obj.save(using=self.db)
         return user_obj
 
-    def create_staffuser(self,email,password):
+    def create_staffuser(self,email,full_name=None,password=None):
         user = self.create_user(
             email,
+            full_name=full_name,
             password=password,
             is_staff=True
         )
         return user
 
-    def create_superuser(self,email,password):
+    def create_superuser(self,email,full_name=None,password=None):
         user = self.create_user(
             email,
+            full_name=full_name,
             password=password,
             is_staff=True,
             is_admin=True
@@ -39,6 +42,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser):
     email     = models.EmailField(max_length=255,unique=True)
+    full_name = models.CharField(max_length=255,blank=True,null=True)
     active    = models.BooleanField(default=True)
     staff     = models.BooleanField(default=False)
     admin     = models.BooleanField(default=False)
@@ -52,13 +56,15 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.email
     def get_full_name(self):
+        if(self.full_name):
+            return self.full_name
         return self.email
     def get_short_name(self):
         return self.email
     def has_perm(self,perm,obj=None):
         return True
     def has_module_perms(self,app_label):
-        return True        
+        return True
 
     @property
     def is_staff(self):
