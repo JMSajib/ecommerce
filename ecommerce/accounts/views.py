5c472f7f659f5ecaa1e1fa8be05f8f7596087d68
JMSajib <jm.sajib012@gmail.com>
2018-04-01 01:36:54 +0600

    UserSession model created with signals

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,get_user_model
 from django.views.generic import CreateView,FormView
+from .signals import user_logged_in
 from django.http import HttpResponse
 from django.utils.http import is_safe_url
 from .forms import LoginForm,RegisterForm,GuestForm
@@ -42,6 +43,7 @@ class LoginView(FormView):
         user = authenticate(request, username=email, password=password)
         if(user is not None):
             login(request,user)
+            user_logged_in.send(user.__class__,instance=user,request=request)
             try:
                 del request.session['guest_email_id']
             except:
