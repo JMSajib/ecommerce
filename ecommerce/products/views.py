8739d74053ec75097114939c0c0b12e1490833f9
JMSajib <jm.sajib012@gmail.com>
2018-03-31 23:42:27 +0600

    Created custom signals & custom mixins

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import ListView,DetailView
 from products.models import Product
+from analytics.mixins import ObjectViewedMixin
 from carts.models import Cart
 # Create your views here.
 class ProductListView(ListView):
@@ -21,11 +22,11 @@ class ProductFeaturedListView(ListView):
         request = self.request
         return Product.objects.featured()
 
-class ProductFeaturedDetailView(DetailView):
+class ProductFeaturedDetailView(ObjectViewedMixin,DetailView):
     queryset = Product.objects.featured()
     template_name = "products/detail.html"
 
-class ProductDetailView(DetailView):
+class ProductDetailView(ObjectViewedMixin,DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
 
@@ -43,8 +44,9 @@ class ProductDetailView(DetailView):
             raise Http404('Products Not Found')
         return instance
 
-class ProductDetailSlugView(DetailView):
+class ProductDetailSlugView(ObjectViewedMixin,DetailView):
     queryset = Product.objects.all()
+    template_name = "products/detail.html"
 
     def get_context_data(self,*args,**kwargs):
         context = super(ProductDetailSlugView,self).get_context_data(*args,**kwargs)
@@ -52,4 +54,18 @@ class ProductDetailSlugView(DetailView):
         context['cart'] = cart_obj
         return context
 
-    template_name = "products/detail.html"
+    def get_object(self,*args,**kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+        try:
+            instance = Product.objects.get(slug=slug,active=True)
+        except Product.DoesNotExist:
+            raise Http404("Not found")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug,active=True)
+            instance = qs.first()
+        except:
+            raise Http404("Problem")
+
+        # object_viewed_signal.send(instance.__class__,instance=instance,request=request)
+        return instance
